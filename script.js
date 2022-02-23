cc128bf621773ad3d3344379b44fdde886501100
Abijahford <ford.abijah@gmail.com>
2022-02-23 17:52:54 -0600

    Add JS functionality, hide start menu

---
+++ script.js
@@ -0,0 +1,14 @@
+// Open Start Menu when Start button is clicked
+document.querySelector('.start-button').onclick = function () {
+  this.classList.toggle('clicked-start-button');
+  document.querySelector('.start-menu').classList.toggle('hidden');
+};
+// Hide Start Menu if it has no class of hidden and main is clicked
+const startButton = document.querySelector('.start-button');
+const startMenu = document.querySelector('.start-menu');
+document.querySelector('.main').onclick = function () {
+  if (!startMenu.classList.contains('hidden')) {
+    startButton.classList.toggle('clicked-start-button');
+    startMenu.classList.add('hidden');
+  }
+};
